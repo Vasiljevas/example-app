80c29eeda4c909654a70cf0fc01cd4e3373d3670
Andrius <vasiljevas.taog@gmail.com>
2021-10-23 13:20:18 +0300

    fixx

---
+++ src/services/cards.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/cards";
+
+export const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+export const create = async (newObject) => {
+  return await axios.post(baseUrl, newObject).data;
+};
+
+export const update = async (id, newObject) => {
+  return await axios.put(`${baseUrl}/${id}`, newObject).data;
+};
